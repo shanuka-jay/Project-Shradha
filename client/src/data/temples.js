81ffff505b8c37d9b82427fc0aed3475a40a2ea4
Tharusha Dineth <tdineth53@gmail.com>
2026-05-05 22:22:38 +0530

    Create temples.js
    list of temple data

---
+++ client/src/data/temples.js
@@ -0,0 +1,35 @@
+export const temples = [
+    {
+        id: 1,
+        name: "Saddha Buddhist Center",
+        state: "Kansas",
+        address: "5017, 16th St, NW, Washington DC, 20011, USA",
+        history: "A Buddhist temple serving the community in Washington DC.",
+        imageUrl: "https://via.placeholder.com/300x180",
+    },
+    {
+        id: 2,
+        name: "California Buddhist Temple",
+        state: "California",
+        address: "Los Angeles, California, USA",
+        history: "A temple located in California.",
+        imageUrl: "https://via.placeholder.com/300x180",
+    },
+
+    {
+        id: 2,
+        name: "SL Buddhist Temple",
+        state: "California",
+        address: "Los Angeles, California, USA",
+        history: "A temple located in California.",
+        imageUrl: "https://via.placeholder.com/300x180",
+    },
+    {
+        id: 3,
+        name: "New York Buddhist Temple",
+        state: "New York",
+        address: "New York, USA",
+        history: "A temple located in New York.",
+        imageUrl: "https://via.placeholder.com/300x180",
+    },
+];
