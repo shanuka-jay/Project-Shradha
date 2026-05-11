e769a25a1e3f09c4dc907061ba043c29f33dcf12
Tharusha Dineth <tdineth53@gmail.com>
2026-05-11 10:57:22 +0530

    Update Massages.jsx
    Corrected typo

--- admin/src/pages/Massages.jsx
+++ admin/src/pages/Massages.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useAuth } from '../context/AuthContext'
-import './Messages.css'
+import './Massages.css'
 
 export default function Messages() {
   const { token } = useAuth()
